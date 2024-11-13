11116c3627def84d093dc26f1f098aa1c0cb9a98
Yesenia-da <yeseniamicanquer760@gmail.com>
2024-11-13 16:06:18 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let hora = new Date().getHours(); 
+if (hora >= 6 && hora < 12) {
+    document.write("Buenos días");
+} else if (hora >= 12 && hora < 18) {
+    document.write("Buenas tardes");
+} else {
+    document.write("Buenas noches");
+}
